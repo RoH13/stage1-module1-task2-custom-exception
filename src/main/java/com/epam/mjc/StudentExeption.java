9789d7cba615850b48a9939e00053fa241b16023
raman <r4m4n911@gmail.com>
2025-07-11 23:07:38 +0300

    fdsf

---
+++ src/main/java/com/epam/mjc/StudentExeption.java
@@ -0,0 +1,6 @@
+package com.epam.mjc;
+public class StudentExeption extends IllegalArgumentException{
+    public StudentExeption(String str) {
+        super(str);
+    }
+}
--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,20 +1,30 @@
 package com.epam.mjc;
 
+import java.util.Arrays;
 
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
+  public Student find(long studentID) throws StudentExeption{
+    if (Student.getValueOf(studentID) == null) {
+      throw new StudentExeption("Could not find student with ID" + " {" + studentID + "}");
+    }
     return Student.getValueOf(studentID);
+  
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+      try { 
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+      } catch(StudentExeption e) {
+        System.err.println(e);
+      }
+      
     }
 
   }
